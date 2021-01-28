b6d6cd14f791f826f4a471f8362268f983ebb425
GLS 02 <traoreibrahima2708>
2021-01-28 15:06:41 +0100

    suppresion fichier component

--- bower_components/validator-js/src/lib/isBIC.js
+++
@@ -1,8 +0,0 @@
-import assertString from './util/assertString';
-
-const isBICReg = /^[A-z]{4}[A-z]{2}\w{2}(\w{3})?$/;
-
-export default function isBIC(str) {
-  assertString(str);
-  return isBICReg.test(str);
-}
